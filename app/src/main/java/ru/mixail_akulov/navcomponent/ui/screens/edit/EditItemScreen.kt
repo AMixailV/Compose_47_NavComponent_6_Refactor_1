ab76ecbf333ed3613b54200b044ba1f02a36275f
AMixailV <Mixer-X@mail.ru>
2024-10-14 20:36:28 +0300

    Initial commit

---
+++ app/src/main/java/ru/mixail_akulov/navcomponent/ui/screens/edit/EditItemScreen.kt
@@ -0,0 +1,74 @@
+package ru.mixail_akulov.navcomponent.ui.screens.edit
+
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
+import ru.mixail_akulov.navcomponent.R
+import ru.mixail_akulov.navcomponent.model.LoadResult
+import ru.mixail_akulov.navcomponent.ui.screens.edit.EditItemViewModel.ScreenState
+import ru.mixail_akulov.navcomponent.ui.screens.EditItemRoute
+import ru.mixail_akulov.navcomponent.ui.EventConsumer
+import ru.mixail_akulov.navcomponent.ui.components.ItemDetails
+import ru.mixail_akulov.navcomponent.ui.components.ItemDetailsState
+import ru.mixail_akulov.navcomponent.ui.components.LoadResultContent
+import ru.mixail_akulov.navcomponent.ui.screens.LocalNavController
+import ru.mixail_akulov.navcomponent.ui.screens.routeClass
+
+@Composable
+fun EditItemScreen(index: Int) {
+    val viewModel = hiltViewModel<EditItemViewModel, EditItemViewModel.Factory> { factory ->
+        factory.create(index)
+    }
+    val navController = LocalNavController.current
+    EventConsumer(channel = viewModel.exitChannel) {
+        if (navController.currentBackStackEntry.routeClass() == EditItemRoute::class) {
+            navController.popBackStack()
+        }
+    }
+    val loadResult by viewModel.stateFlow.collectAsState()
+    EditItemContent(
+        loadResult = loadResult,
+        onEditButtonClicked = viewModel::update
+    )
+}
+
+@Composable
+fun EditItemContent(
+    loadResult: LoadResult<ScreenState>,
+    onEditButtonClicked: (String) -> Unit
+) {
+    LoadResultContent(
+        loadResult = loadResult,
+        content = { screenState ->
+            SuccessEditItemContent(
+                state = screenState,
+                onEditButtonClicked = onEditButtonClicked
+            )
+        }
+    )
+}
+
+@Composable
+private fun SuccessEditItemContent(
+    state: ScreenState,
+    onEditButtonClicked: (String) -> Unit
+) {
+    ItemDetails(
+        state = ItemDetailsState(
+            loadedItem = state.loadedItem,
+            textFieldPlaceholder = stringResource(id = R.string.enter_new_item),
+            actionButtonText = stringResource(id = R.string.add),
+            isActionInProgress = state.isEditInProgress
+        ),
+        onActionButtonClicked = onEditButtonClicked,
+        modifier = Modifier.fillMaxSize()
+    )
+}
---
+++ app/src/main/java/ru/mixail_akulov/navcomponent/ui/screens/edit/EditItemViewModel.kt
@@ -0,0 +1,66 @@
+package ru.mixail_akulov.navcomponent.ui.screens.edit
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import ru.mixail_akulov.navcomponent.model.ItemsRepository
+import ru.mixail_akulov.navcomponent.model.LoadResult
+
+@HiltViewModel(assistedFactory = EditItemViewModel.Factory::class)
+class EditItemViewModel @AssistedInject constructor(
+    @Assisted private val index: Int,
+    private val itemsRepository: ItemsRepository
+) : ViewModel() {
+
+    private val _stateFlow = MutableStateFlow<LoadResult<ScreenState>>(LoadResult.Loading)
+    val stateFlow: StateFlow<LoadResult<ScreenState>> = _stateFlow
+
+    private val _exitChannel = Channel<Unit>()
+    val exitChannel: ReceiveChannel<Unit> = _exitChannel
+
+
+    data class ScreenState(
+        val loadedItem: String,
+        val isEditInProgress: Boolean = false
+    )
+
+    init {
+        viewModelScope.launch {
+            val loadedItem = itemsRepository.getByIndex(index)
+            _stateFlow.value = LoadResult.Success(ScreenState(loadedItem))
+        }
+    }
+
+    fun update(newValue: String) {
+        val loadResult = _stateFlow.value
+        if (loadResult !is LoadResult.Success) return
+        viewModelScope.launch {
+            showProgress(loadResult)
+            itemsRepository.update(index, newValue)
+            goBack()
+        }
+    }
+
+    private fun showProgress(loadResult: LoadResult.Success<ScreenState>) {
+        val currentScreenState = loadResult.data
+        val updatedScreenState = currentScreenState.copy(isEditInProgress = true)
+        _stateFlow.value = LoadResult.Success(updatedScreenState)
+    }
+
+    private suspend fun goBack() {
+        _exitChannel.send(Unit)
+    }
+
+    @AssistedFactory
+    interface Factory {
+        fun create(index: Int): EditItemViewModel
+    }
+}
